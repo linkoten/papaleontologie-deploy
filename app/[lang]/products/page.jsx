5afcc41846749255b821c1356061f88533f9fc7e
Catto Francois <catto.francois@hotmail.fr>
2023-11-16 10:29:10 +0100

    quelques maj

--- app/[lang]/products/page.jsx
+++ app/[lang]/products/page.jsx
@@ -11,12 +11,12 @@ export default cache(async function Page({
   params : { lang }
 }) {
 const {page } = await getDictionary(lang);
-const {results: attributes} = await getAttributes ({ page: 1 })
-const {results: products1} = await getProducts ({ page: 1 })
-const {results: products2} = await getProducts2 ({ page: 2 })
-const {results: products3} = await getProducts3 ({ page: 3 })
-const {results: products4} = await getProducts4 ({ page: 4 })
-const {results: products5} = await getProducts5 ({ page: 5 })
+const {results: attributes} = await getAttributes ({ page: 1, revalidate: 60 })
+const {results: products1} = await getProducts ({ page: 1, revalidate: 60  })
+const {results: products2} = await getProducts2 ({ page: 2, revalidate: 60  })
+const {results: products3} = await getProducts3 ({ page: 3, revalidate: 60  })
+const {results: products4} = await getProducts4 ({ page: 4, revalidate: 60  })
+const {results: products5} = await getProducts5 ({ page: 5, revalidate: 60  })
 
 const products = [].concat(products1, products2, products3, products4, products5);
 
